e3cf1cba6cc6a6df5b069cd8ffee97b307648acb
David Lu <davidvlu@gmail.com>
2015-07-21 17:46:41 -0400

    Subscriptions

--- include/path_costmap_layer/path_costmap_layer.h
+++ include/path_costmap_layer/path_costmap_layer.h
@@ -1,15 +1,29 @@
 #ifndef PATH_COSTMAP_LAYER_H_
 #define PATH_COSTMAP_LAYER_H_
 #include <ros/ros.h>
-#include <costmap_2d/layer.h>
+#include <costmap_2d/costmap_layer.h>
 #include <costmap_2d/layered_costmap.h>
 #include <costmap_2d/GenericPluginConfig.h>
 #include <dynamic_reconfigure/server.h>
+#include <nav_msgs/Path.h>
 
 namespace path_costmap_layer
 {
+class PathLayer;
 
-class PathLayer : public costmap_2d::Layer, public costmap_2d::Costmap2D
+class PathSubscriber
+{
+public:
+    PathSubscriber(ros::NodeHandle nh, PathLayer* main, std::string topic, unsigned int id);
+
+    void incomingPath(const nav_msgs::PathConstPtr& message);
+private:
+    unsigned int id_;
+    PathLayer* main_;
+    ros::Subscriber subscriber_;
+};
+
+class PathLayer : public costmap_2d::CostmapLayer
 {
 public:
   PathLayer();
@@ -25,7 +39,11 @@ public:
 
   virtual void matchSize();
 
+  void incomingPath(const nav_msgs::PathConstPtr& message, int id);
+
 private:
+  std::vector< ros::Subscriber > subscribers_;
+
   void reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level);
   dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig> *dsrv_;
 };
--- src/path_costmap_layer.cpp
+++ src/path_costmap_layer.cpp
@@ -8,6 +8,15 @@ using costmap_2d::NO_INFORMATION;
 
 namespace path_costmap_layer
 {
+    
+PathSubscriber::PathSubscriber(ros::NodeHandle nh, PathLayer* main, std::string topic, unsigned int id) : main_(main), id_(id) {
+    subscriber_ = nh.subscribe(topic, 1, &PathSubscriber::incomingPath, this);
+}
+
+void PathSubscriber::incomingPath(const nav_msgs::PathConstPtr& message)
+{
+    main_->incomingPath(message, id_);
+}
 
 PathLayer::PathLayer() {}
 
@@ -17,6 +26,13 @@ void PathLayer::onInitialize()
   current_ = true;
   default_value_ = NO_INFORMATION;
   matchSize();
+  
+  std::vector<std::string> topic_list;
+  nh.getParam("path_topics", topic_list);
+  for(unsigned i=0; i < topic_list.size(); i++) {
+      ROS_INFO("TOPIC: %s", topic_list[i].c_str());
+      new PathSubscriber(nh, this, topic_list[i], i);
+  }
 
   dsrv_ = new dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>(nh);
   dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>::CallbackType cb = boost::bind(
@@ -38,23 +54,28 @@ void PathLayer::reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t 
   enabled_ = config.enabled;
 }
 
+void PathLayer::incomingPath(const nav_msgs::PathConstPtr& path, int id)
+{
+    for(int i=0;i<path->poses.size(); i++){
+        double x = path->poses[i].pose.position.x, 
+               y = path->poses[i].pose.position.y;
+        
+        unsigned int mx;
+        unsigned int my;
+        if(!worldToMap(x, y, mx, my)){
+            continue;
+        }
+        addExtraBounds(x,y,x,y);
+        setCost(mx,my, LETHAL_OBSTACLE);
+    }
+}
+
 void PathLayer::updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x,
                                            double* min_y, double* max_x, double* max_y)
 {
   if (!enabled_)
     return;
-
-  double mark_x = robot_x + cos(robot_yaw), mark_y = robot_y + sin(robot_yaw);
-  unsigned int mx;
-  unsigned int my;
-  if(worldToMap(mark_x, mark_y, mx, my)){
-    setCost(mx, my, LETHAL_OBSTACLE);
-  }
-  
-  *min_x = std::min(*min_x, mark_x);
-  *min_y = std::min(*min_y, mark_y);
-  *max_x = std::max(*max_x, mark_x);
-  *max_y = std::max(*max_y, mark_y);
+  useExtraBounds(min_x, min_y, max_x, max_y);
 }
 
 void PathLayer::updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i,
